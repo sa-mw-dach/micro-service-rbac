ecef65e64a5d6bd742430b30e9999b29898f3f39
Stephan Kraft <skraft@redhat.com>
2023-03-31 20:19:17 +0200

    importing RequestHandler

--- base/servicea/src/main/java/org/acme/ExternalService.java
+++ base/servicea/src/main/java/org/acme/ExternalService.java
@@ -3,6 +3,7 @@ package org.acme;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient
